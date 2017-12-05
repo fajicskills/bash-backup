4612e3dd5e695d87afe727ffa59cd8710f6967ac
Ange Bagui <angebagui@gmail.com>
2017-12-05 19:56:50 +0000

    MongoDb Dump

--- backup.sh
+++ backup.sh
@@ -39,7 +39,7 @@ backup_files="/root/.bash_history /etc/passwd"
 # Directories to backup (Multi value)
 backup_dir_enable="yes"
 #backup_directories="/etc /var/log /usr/local"/var/www/mongodb_backup_dir
-mongodb_backup_dir= "/var/www/mongodb_backup_dir_$(date +"%Y-%m-%d-%H-%M-%S")"
+mongodb_backup_dir= "/var/www/mongodb_backup_dir_$(date +"%Y_%m_%d_%H_%M_%S")"
 
 mkdir "$mongodb_backup_dir"
 
